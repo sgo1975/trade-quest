7bf691fd49c4fb94026894581e63ac4ef5e9fddc
sgo1975 <svengottschalk@gmail.com>
2026-03-17 10:48:22 +0100

    add github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { VitePWA } from 'vite-plugin-pwa'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
-      manifest: {
-        name: 'TradeQuest RPG',
-        short_name: 'TradeQuest',
-        description: 'Gamified Crypto Portfolio Tracker',
-        theme_color: '#0f172a', // Slate-900 (dein Hintergrund)
-        background_color: '#0f172a',
-        display: 'standalone', // Lässt die Browser-Leiste verschwinden
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
   ],
+  // WICHTIG: Muss deinem Projektnamen in GitLab entsprechen
+  base: '/trade-quest/', 
 })
